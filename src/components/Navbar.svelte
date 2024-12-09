eb2000e6b74c75f4cce1738173d413b570c196cf
Carlos Humberto Torres Endo <22crls01@gmail.com>
2024-12-09 10:19:53 +0100

    Tercer commit: Creación de NavBar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let links = [
+    { name: "Nike", href: "#" },
+    { name: "Jordan 1", href: "#" },
+    { name: "Jordan 4", href: "#" },
+    { name: "Nike Dunk", href: "#" },
+    { name: "Off-White", href: "#" },
+  ];
+</script>
+
+<nav class="navbar">
+  <h1>SNEAKVERSE</h1>
+  <ul>
+    {#each links as link}
+      <li><a href={link.href}>{link.name}</a></li>
+    {/each}
+  </ul>
+</nav>
+
+<style>
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem;
+    background: black;
+    color: white;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  li {
+    margin: 0 1rem;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+</style>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Navbar from "../components/Navbar.svelte";
+</script>
+
+<Navbar />
+
+<h1>Ediciones limitadas</h1>
+<div class="grid"></div>
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    gap: 1rem;
+    padding: 1rem;
+  }
+</style>
